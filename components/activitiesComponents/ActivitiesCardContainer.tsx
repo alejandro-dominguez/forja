fb5a96718da8163e74eefdc31f39af91da9b18fe
Alejandro Dominguez <alejandro.dominguez1@live.com>
2025-12-28 17:38:34 -0300

    updated activities page styling

--- components/activitiesComponents/ActivitiesCardContainer.tsx
+++ components/activitiesComponents/ActivitiesCardContainer.tsx
@@ -4,16 +4,9 @@ import { activities } from './activities.data';
 const ActivitiesCardContainer = () => {
     return (
         <section>
-            <div
-                className='
-                    flex gap-7 overflow-x-auto pb-6 snap-x snap-mandatory sm:hidden
-                    scrollbar-thin scrollbar-thumb-main/80 scrollbar-track-darker/40'
-            >
+            <div className='flex gap-7 overflow-x-auto pb-6 snap-x snap-mandatory sm:hidden'>
                 {activities.map(activity => (
-                    <div
-                        key={activity.id}
-                        className='min-w-[85%] snap-center'
-                    >
+                    <div key={activity.id} className='min-w-[85%] snap-center'>
                         <ActivityCard activity={activity} />
                     </div>
                 ))}
