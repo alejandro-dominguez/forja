a9c6d2d6b1f7fb90fc9bb8af6d571da875ac1a7d
Alejandro Dominguez <alejandro.dominguez1@live.com>
2025-12-30 17:32:03 -0300

    updated BenefirPromos styling

--- components/benefitsComponents/benefitsInfoComponents/BenefitPromos.tsx
+++ components/benefitsComponents/benefitsInfoComponents/BenefitPromos.tsx
@@ -2,19 +2,19 @@ const BenefitPromos = () => {
     return (
         <div
             className='grid place-items-center text-center font-dela-gothic-one relative
-            pb-8 rounded shadow shadow-darker/50 bg-cover bg-center bg-no-repeat mt-5'
+            pb-9 rounded shadow shadow-darker/50 bg-cover bg-center bg-no-repeat mt-5'
             style={{ backgroundImage: `url('https://i.ibb.co/FLbXtvcV/fondo-contacto.png')` }}
         >
             <div className='absolute inset-0 bg-darker/60 rounded' />
             <h3
-                className='text-lighter mt-5 font-dela-gothic-one
+                className='text-lighter mt-6 font-dela-gothic-one
                 text-2xl sm:text3xl uppercase z-10'
             >
                 PROMOCIONES
             </h3>
             <div
-                className='px-4 sm:px-8 py-3 grid place-items-center gap-1
-                border-2 border-light rounded mt-2 z-10 text-lg'
+                className='px-4 sm:px-8 pt-3 pb-4 grid place-items-center gap-1
+                border-2 border-light rounded mt-4 z-10 text-lg'
             >
                 <span className='text-lighter'>
                     10%
