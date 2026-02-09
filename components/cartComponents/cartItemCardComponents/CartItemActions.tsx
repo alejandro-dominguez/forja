ba723e013e15f57a4161eb334089d3fdbdc45797
Alejandro Dominguez <alejandro.dominguez1@live.com>
2026-02-08 22:46:26 -0300

    updated cartItemActions component

--- components/cartComponents/cartItemCardComponents/CartItemActions.tsx
+++ components/cartComponents/cartItemCardComponents/CartItemActions.tsx
@@ -4,24 +4,30 @@ import { useCart } from '@/context/CartContext';
 import { BiSolidMinusCircle, BiSolidPlusCircle, BiSolidTrash, } from 'react-icons/bi';
 
 const CartItemActions = ({ quantity, id }: { quantity: number; id: number }) => {
-    const { increaseQty, decreaseQty, removeProduct } = useCart();
+    const { increaseQty, decreaseQty, removeProduct } = useCart()
 
     return (
         <div className='flex items-center justify-between mt-4 sm:mt-5'>
             <div className='flex items-center gap-4 text-white'>
-                <button onClick={() => decreaseQty(id)}>
+                <button
+                    onClick={() => decreaseQty(id)}
+                    className='cursor-pointer'
+                >
                     <BiSolidMinusCircle size={22} />
                 </button>
                 <span className='font-medium text-lg w-6 text-center'>
                     {quantity}
                 </span>
-                <button onClick={() => increaseQty(id)}>
+                <button
+                    onClick={() => increaseQty(id)}
+                    className='cursor-pointer'
+                >
                     <BiSolidPlusCircle size={22} />
                 </button>
             </div>
             <button
                 onClick={() => removeProduct(id)}
-                className='text-white'
+                className='text-white cursor-pointer'
             >
                 <BiSolidTrash size={22} />
             </button>
